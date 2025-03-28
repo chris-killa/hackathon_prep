46eea22f523d8b30502dd69c6ea9d4925a124a53
hecking_killa <chris.hermann9397@gmail.com>
2025-03-28 10:22:28 +0100

    main logic

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import Peyman
+import Chris
+import martin
+def main():
+    username = Peyman.get_username()
+    adding = Chris.add_at_symbol_to_username(username)
+    full_mail = martin.add_domain_to_username(adding)
+    print(full_mail)
+main()
