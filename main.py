fa973da52351a428d11a98ea515857a6fb5216f7
Martin Enke <trickform.info@gmail.com>
2025-03-28 15:43:23 +0100

    mvp

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import Peyman
-import Chris
-import martin
+from Peyman import get_username
+from chioma import remove_special_characters
+from Chris import add_at_symbol_to_username
+from martin import add_domain_to_username, display_email_address
+
+
 def main():
-    username = Peyman.get_username()
-    adding = Chris.add_at_symbol_to_username(username)
-    full_mail = martin.add_domain_to_username(adding)
-    print(full_mail)
-main()
+    username = get_username()
+    clean_username = remove_special_characters(username)
+    add_at_symbol = add_at_symbol_to_username(clean_username)
+    add_domain = add_domain_to_username(add_at_symbol)
+    display_email_address(add_domain)
+
+
+if __name__ == "__main__":
+    main()
--- martin.py
+++ martin.py
@@ -1,5 +1,15 @@
 def add_domain_to_username(username):
-    """Adds a domain to the username and the symbol"""
+    """ takes username as an argument and adds
+	a domain name symbol to the username and return the email
+	Example: if the username is user123@
+	         the function should return user123@email.com
+	"""
 
     return username + "email.com"
+
+
+def display_email_address(email):
+    """ display the email """
+    print(email)
+
 
